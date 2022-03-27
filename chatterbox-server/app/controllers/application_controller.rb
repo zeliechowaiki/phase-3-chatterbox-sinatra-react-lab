ba840cd8da20e49716044f8c44dc1f7eae3a1936
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-03-27 19:36:35 -0400

    all deliverables

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,29 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do
+    messages = Message.all
+    messages.to_json
+  end
+
+  post '/messages' do
+    message = Message.create(
+      username: params[:username],
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(body: params[:body])
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
   
 end
